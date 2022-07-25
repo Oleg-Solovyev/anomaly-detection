6f15dee6d9848d83eccb4ca956d9ef3d45061883
Oleg S <oleg.s.solovyev@gmail.com>
2022-07-25 21:11:57 +0400

    added SARIMA script

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Freezer temperature anomaly detector. Calculates time intervals when the tempera
 make_model.py calculates time intervals when the temperature crossess predefined threshold on the training data.
 test_model.py marks anomalies in the data where time intervals are shorter or longer then the intervals in the training data.
 moving_average_forecast.py forecast with moving average
+SARIMA_forecast.py forecast with SARIMA model
 
 
 ### Test
@@ -61,12 +62,34 @@ Run python3 moving_average_forecast.py 4peeks-short.csv
 2022-07-23 21:34:21.708019 mean time interval 36.0
 2022-07-23 21:34:21.708349 Linux time increment is 600
 2022-07-23 21:34:21.708369 Temperature forecast 
-1657939800,-17.866666666666664
-1657940400,-17.73333333333333
-1657941000,-17.85
-1657941600,-17.75
-1657942200,-17.816666666666666
-1657942800,-17.766666666666666
-
+1657940400,-17.866666666666664
+1657941000,-17.73333333333333
+1657941600,-17.85
+1657942200,-17.75
+1657942800,-17.816666666666666
+1657943400,-17.766666666666666
+```
+Run python3 SARIMA_forecast.py 4peeks-short.csv
+```
+> python3 SARIMA_forecast.py 4peeks-short.csv
+2022-07-25 17:01:31.322512 imported file 4peeks-short.csv
+2022-07-25 17:01:31.322804 temperature cutoff is -15.4
+2022-07-25 17:01:31.325234 found 6 intervals crossing -15.4 cutoff
+2022-07-25 17:01:31.325408 mean time interval 36.0
+RUNNING THE L-BFGS-B CODE
+...
+CONVERGENCE: REL_REDUCTION_OF_F_<=_FACTR*EPSMCH             
+2022-07-25 17:03:14.681297 found best SARIMA model with aic=41.57 params=(1, 0, 1)x(1, 1, 1, 36)
+RUNNING THE L-BFGS-B CODE
+...
+CONVERGENCE: REL_REDUCTION_OF_F_<=_FACTR*EPSMCH             
+2022-07-25 17:03:20.825332 Linux time increment is 600
+2022-07-25 17:03:20.825854 Temperature forecast 
+1657940400,-17.550353958500153
+1657941000,-18.06113014552193
+1657941600,-17.660904347833668
+1657942200,-17.832430238422294
+1657942800,-17.903417088591198
+1657943400,-17.58495752638998
 ```
 
--- SARIMA_forecast.py
+++ SARIMA_forecast.py
@@ -6,6 +6,7 @@
 calculates temperature cutoff that is mean of max and min temperature
 calculates time intervals when temperature crosses the cutoff
 calculates mean of time interval value
+loops over SARIMA models and pics the best model
 calculates Linux time increment for output
 calculates forecast as average of temp values at every mean time interval
 
@@ -20,7 +21,11 @@ __status__ = "Prototype"
 
 import sys
 import datetime
+import numpy as np
 import pandas as pd
+import itertools
+import warnings
+import statsmodels.api as sm
 
 
 # get input arguments or use testing values
@@ -53,6 +58,37 @@ mean_cutoff_intervals = cutoff_intervals.mean().round()
 print(str(datetime.datetime.now()) + ' mean time interval ' + str(mean_cutoff_intervals))
 
 
+# loops over SARIMA models and pics the best model
+import itertools
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 36) for x in list(itertools.product(p, d, q))]
+
+warnings.filterwarnings("ignore") 
+results = []
+best_aic = np.nan
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(endog=df.Temperature,
+                                            trend='n',
+                                            order=param,
+                                            seasonal_order=param_seasonal)
+            res = mod.fit()
+            results.append([param, param_seasonal, res.aic])
+            if np.isnan(best_aic) or best_aic > res.aic:
+                best_aic, best_param, best_param_seasonal = res.aic, param, param_seasonal
+        except:
+            continue
+
+print(str(datetime.datetime.now()) + ' found best SARIMA model with aic=' + str(round(best_aic,2)) + ' params=' + str(best_param) + 'x' + str(best_param_seasonal))
+mod = sm.tsa.statespace.SARIMAX(endog=df.Temperature,
+                                            trend='n',
+                                            order=best_param,
+                                            seasonal_order=best_param_seasonal)
+res = mod.fit()
+forecast = res.forecast(steps = 6)
+
 # calculates Linux time increment for output
 time_increment = int(df.LinuxTime.diff().mean())
 print(str(datetime.datetime.now()) + ' Linux time increment is ' + str(time_increment))
@@ -60,5 +96,4 @@ print(str(datetime.datetime.now()) + ' Linux time increment is ' + str(time_incr
 # calculates forecast as average of temp values at every mean time interval
 print(str(datetime.datetime.now()) + ' Temperature forecast ')
 for i in range(0, 6):
-  print(str(int(df.LinuxTime.tail(1) + (1+i)*time_increment)) + ',' + str(df.Temperature[range(df.shape[0]%36+i, df.shape[0], 36)].mean()))
-
+  print(str(int(df.LinuxTime.tail(1) + (1+i)*time_increment)) + ',' + str(forecast[df.shape[0]+i]))
